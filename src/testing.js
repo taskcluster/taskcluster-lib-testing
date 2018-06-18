fbce06b764a5cf25c64b1f8910159d0e625a32ae
Dustin J. Mitchell <dustin@mozilla.com>
2018-06-18 14:56:24 -0400

    always pass a delay to sleep

--- src/testing.js
+++ src/testing.js
@@ -25,19 +25,21 @@ exports.sleep = sleep;
  * Poll a function that returns a promise until the promise is resolved without
  * errors. Poll `iterations` number of times, with `delay` ms in between.
  *
- * Defaults to 16 iterations with a delay of 250 ms.
+ * Defaults to 20 iterations with a delay of 250 ms (total of four seconds).
  *
  * Return a promise that a promise form the `poll` function resolved without
  * error. This will return the first successful poll, and stop polling.
  */
 var poll = function(doPoll, iterations, delay) {
+  delay = delay || 250;
+  iterations = iterations === undefined ? 20 : iterations;
   return doPoll().catch(function(err) {
     // Re-throw
     if (iterations != undefined && iterations <= 0) {
       throw err;
     }
     return sleep(delay).then(function() {
-      return poll(doPoll, (iterations || 20) - 1, delay || 250);
+      return poll(doPoll, iterations - 1, delay);
     });
   });
 };
