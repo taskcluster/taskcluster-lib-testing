faee1546e1f879ae2785e9b7692a8db8711fdb76
Dustin J. Mitchell <dustin@mozilla.com>
2018-06-22 14:53:45 +0000

    Bug 1470362 - fix tests to not affect one another

--- test/secrets_test.js
+++ test/secrets_test.js
@@ -1,24 +1,32 @@
 const {Secrets, stickyLoader} = require('../');
+const _ = require('lodash');
 const assert = require('assert');
 const nock = require('nock');
 
-let old_TASK_ID;
-const clearEnv = () => {
-  old_TASK_ID = process.env.TASK_ID;
-  delete process.env.TASK_ID;
-  delete process.env.PASS_IN_ENV;
-  delete process.env.PASS_IN_BOTH;
-};
-
-const resetEnv = () => {
-  process.env.TASK_ID = old_TASK_ID;
-  delete process.env.PASS_IN_ENV;
-  delete process.env.PASS_IN_BOTH;
-};
+const savedEnv = _.cloneDeep(process.env);
 
 suite('Secrets', function() {
-  suiteSetup(clearEnv);
-  suiteTeardown(resetEnv);
+  let oldTaskId;
+  let savedEnv;
+
+  suiteSetup(function() {
+    // make sure $TASK_ID isn't set for the duration of this suite
+    oldTaskId = process.env.TASK_ID;
+    delete process.env.TASK_ID;
+    savedEnv = _.cloneDeep(process.env);
+  });
+
+  teardown(function() {
+    // reset process.env back to savedEnv, in-place (without $TASK_ID)
+    Object.keys(process.env).forEach(k => delete process.env[k]);
+    Object.entries(savedEnv).forEach(([k, v]) => process.env[k] = v);
+  });
+
+  suiteTeardown(function() {
+    if (oldTaskId) {
+      process.env.TASK_ID = oldTaskId;
+    }
+  });
 
   const loader = (component, overwrites) => {
     if (component in overwrites) {
@@ -57,7 +65,6 @@ suite('Secrets', function() {
     });
 
     test('with nothing', async function() {
-      sticky.inject('cfg', {});
       await secrets.setup();
       assert(!secrets.have('envOnly'));
       assert.throws(() => secrets.get('envOnly'));
@@ -103,7 +110,6 @@ suite('Secrets', function() {
     test('with env', async function() {
       process.env.PASS_IN_ENV = 'PIE';
       process.env.PASS_IN_BOTH = 'PIB';
-      sticky.inject('cfg', {});
       await secrets.setup();
       assert(secrets.have('envOnly'));
       assert.deepEqual(secrets.get('envOnly'), {PASS_IN_ENV: 'PIE'});
@@ -114,8 +120,8 @@ suite('Secrets', function() {
     });
 
     test('with env via secrets service', async function() {
+      process.env.TASK_ID = 'abc123'; // so fetching occurs
       secrets._fetchSecrets = async () => ({PASS_IN_ENV: 'PIE', PASS_IN_BOTH: 'PIB'});
-      sticky.inject('cfg', {});
       await secrets.setup();
       assert(secrets.have('envOnly'));
       assert.deepEqual(secrets.get('envOnly'), {PASS_IN_ENV: 'PIE'});
